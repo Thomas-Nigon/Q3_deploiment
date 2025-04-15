5e2fea640f197e9c3834c7e7517eef84562dcc67
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2025-04-15 12:23:29 +0200

    redit title

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -31,7 +31,7 @@ function App() {
     <div>
       <Header>
         <Container>
-          <h1>TOTOTOTOTOTOOT</h1>
+          <h1>TOTOTOTOTOTOOT coucou</h1>
         </Container>
       </Header>
       <Container>
