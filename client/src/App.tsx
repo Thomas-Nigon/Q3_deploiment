f4bcb60cabd449a8b3a2e95e85b0e9de1f2136a3
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2025-04-15 11:36:22 +0200

    add h1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,18 +16,20 @@ const ALL_WILDERS = gql`
 `;
 
 export type WilderData = {
-  _id: string
-  name:string
-  city: string
-}
+  _id: string;
+  name: string;
+  city: string;
+};
 
 function App() {
-  
-  const { loading, error, data } = useQuery<{wilders:WilderData[]}>(ALL_WILDERS);
+  const { loading, error, data } = useQuery<{ wilders: WilderData[] }>(
+    ALL_WILDERS
+  );
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
   return (
     <div>
+      <h1>WASSSSSUUUUUUUUUp</h1>
       <Header>
         <Container>
           <h1>Wilders Book with CD</h1>
